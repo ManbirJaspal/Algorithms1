78a02bec02852b02845139a9103557cab3098904
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-09-25 15:19:02 -0400

    Q4 done

---
+++ src/com/Manbir/arraysandstrings/exercise1/Q4.java
@@ -0,0 +1,27 @@
+package com.Manbir.arraysandstrings.exercise1;
+
+public class Q4 {
+//    Write a function that when given a string array outputs “police” when it encounters “cop” and “robber” when it encounters “thief”
+
+    public static void main(String args[]) {
+
+        String[] strArr = new String[]{"cop", "thief"};
+        functionQ4(strArr);
+    }
+
+    private static void functionQ4(String[] strArr) {
+        if ((strArr == null) || (strArr.length == 0)) {
+            System.out.println("empty array");
+        }
+
+        for (String e : strArr) {
+            if (e.equals("cop")) {
+                System.out.print("police \n");
+            }
+
+            if (e.equals("thief")) {
+                System.out.println("robber");
+            }
+        }
+    }
+}
