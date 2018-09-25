fb68a3404e3ef6b10a96732e349df83f8c4da88b
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-09-25 15:36:18 -0400

    Q1 made changes

--- src/com/Manbir/arraysandstrings/exercise1/Q1.java
+++ src/com/Manbir/arraysandstrings/exercise1/Q1.java
@@ -5,21 +5,19 @@ public class Q1 {
 
     public static void main(String[] args) {
 
-        int[] arr = new int[] {1,2,3,5,7,32,5,7,7};
+        int[] arr = new int[]{1, 2, 3, 5, 7, 32, 5, 7, 7};
         System.out.println("The total after the sum is: " + arraySum(arr));
 
     }
 
     private static int arraySum(int[] arr) {
-        if ((arr == null) || (arr.length ==  0)) {
-            return -1;
-        }
-
         int sum = 0;
-        for (int e : arr) {
-            sum = sum + e;
-        }
+        if (arr != null && arr.length != 0) {
+            for (int e : arr) {
+                sum = sum + e;
+            }
 
+        }
         return sum;
     }
 }
