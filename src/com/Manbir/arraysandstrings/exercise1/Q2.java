7fc6df2a0a595701c35aa4875704cbc4d74f9dcb
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-09-25 15:16:58 -0400

    Q2 done

---
+++ src/com/Manbir/arraysandstrings/exercise1/Q2.java
@@ -0,0 +1,24 @@
+package com.Manbir.arraysandstrings.exercise1;
+
+
+//    Write a function that when given an integer array prints only the negative entries
+
+public class Q2 {
+
+    public static void main(String args[]) {
+        int[] arr = new int[]{1, 2, -3, 5, 7, -32, 5, -7, 7};
+        arrayNeg(arr);
+
+    }
+
+    public static void arrayNeg(int[] arr) {
+        if ((arr == null) || (arr.length == 0)) {
+            System.out.println("empty array");
+        }
+        for (int e : arr) {
+            if (e < 0) {
+                System.out.println(e);
+            }
+        }
+    }
+}
