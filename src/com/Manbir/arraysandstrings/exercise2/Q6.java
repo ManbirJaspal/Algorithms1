5a38937f108c53233445c17844740439347931be
manbirsinghjaspal <manb.jaspal@gmail.com>
2018-09-25 15:20:25 -0400

    Q6 done

---
+++ src/com/Manbir/arraysandstrings/exercise2/Q6.java
@@ -0,0 +1,31 @@
+package com.Manbir.arraysandstrings.exercise2;
+
+import java.util.ArrayList;
+
+public class Q6 {
+//    Write a function that takes in an Integer array and returns a list of indexes where null is present.
+
+    public static void main(String[] args) {
+        Integer[] al1 = new Integer[]{1, 23, 2, 1, 3, null, 4, 3, null, null, 23, null};
+        System.out.println(getNullIndex(al1));
+    }
+
+    public static ArrayList<Integer> getNullIndex(Integer[] arr) {
+        if ((arr == null) || (arr.length == 0)) {
+            System.out.println("empty array");
+        }
+
+        int index = 0;
+        ArrayList<Integer> nullIndexList = new ArrayList<Integer>();
+        for (Integer e : arr) {
+            if (e == null) {
+                nullIndexList.add(index);
+                System.out.println("null present at index: " + index);
+            }
+            index++;
+        }
+        return nullIndexList;
+    }
+}
+
+
